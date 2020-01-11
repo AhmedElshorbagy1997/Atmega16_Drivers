ea19e6e1ad11b4cae488988936c7435a5da46164
Ahmed Elshorbagy <59618979+AhmedElshorbagy1997@users.noreply.github.com>
2020-01-11 03:36:31 +0200

    Add files via upload

---
+++ Control the speed of a DC Motor/Code/Debug/Mini_Project_2.d
@@ -0,0 +1,17 @@
+Mini_Project_2.o Mini_Project_2.o: ../Mini_Project_2.c ../adc.h \
+  ../micro_config.h ../std_types.h ../common_macros.h ../pwm.h ../lcd.h \
+  ../interrupt.h
+
+../adc.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../pwm.h:
+
+../lcd.h:
+
+../interrupt.h:
